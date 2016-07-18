0d6964c794c8f0dd5a1b75d13e5799f5c4246994
Chris Ball <chris@echobind.com>
2016-07-18 13:28:06 -0400

    add component and route-action helper

--- app/organizations/route.js
+++ app/organizations/route.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      { id: 1, name: 'Org 1', isStarred: true },
+      { id: 2, name: 'Org 2', isStarred: false }
+    ];
+  },
+
+  actions: {
+    star(organization) {
+      // handle plain js objects or ember objects
+      let starred = Ember.get(organization, 'isStarred');
+      Ember.set(organization, 'isStarred', !starred);
+    }
+  }
 });
